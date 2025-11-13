eeefc45974f9a1c34dd6a830c2f228911a90245e
orohach92 <rohach.oleksandr@gmail.com>
2025-11-13 18:26:45 +0100

    homework12

---
+++ src/test/java/com/rohach/test/homeWork12.java
@@ -0,0 +1,30 @@
+package com.rohach.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class homeWork12 {
+
+    WebDriver driver;
+
+    //before test
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+    }
+    // test
+    @Test
+    public void openBrowser(){
+        System.out.println("Demo Web Shop was opened");
+    }
+
+    //after test
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.close();
+    }
+}
