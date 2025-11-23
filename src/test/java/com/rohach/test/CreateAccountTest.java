f722611cb06964c7c1b500661dac043c2cb9b249
orohach92 <rohach.oleksandr@gmail.com>
2025-11-23 21:42:38 +0100

    homework18

--- src/test/java/com/rohach/test/CreateAccountTest.java
+++ src/test/java/com/rohach/test/CreateAccountTest.java
@@ -10,23 +10,9 @@ public class CreateAccountTest extends TestBase{
     public void newUserRegisterPositiveTest(){
         int i =(int)((System.currentTimeMillis()/1000)%3600);
 
-        //click on Register Link
-        click(By.cssSelector("[href='/register']"));
-        //click on Male gender button
-        click(By.cssSelector("#gender-male"));
-        //enter First name
-        type(By.cssSelector("#FirstName"),"Alex");
-        //enter Last name
-        type(By.cssSelector("#LastName"),"Rohach");
-        //enter email
-        type(By.cssSelector("#Email"),"rohach" + i + "@gmail.com");
-        //enter password
-        type(By.cssSelector("#Password"), "Qwerty123456!");
-        //confirm password
-        type(By.cssSelector("#ConfirmPassword"), "Qwerty123456!");
-
-        //click on Registration button
-        click(By.cssSelector("#register-button"));
+        clickOnRegisterLink();
+        fillRegistrationForm(i, "Alex", "Rohach", "Qwerty123456!");
+        clickOnRegistrationButton();
 
         //assert SigOut button present
         Assert.assertTrue(isElementPresent(By.xpath("//*[@class='result']")));
--- src/test/java/com/rohach/test/TestBase.java
+++ src/test/java/com/rohach/test/TestBase.java
@@ -53,4 +53,62 @@ public class TestBase {
             return true;
         }
     }
+
+    public void clickOnRegistrationButton() {
+        //click on Registration button
+        click(By.cssSelector("#register-button"));
+    }
+
+    public void fillRegistrationForm(int i, String firstName, String lastName, String password) {
+        //click on Male gender button
+        click(By.cssSelector("#gender-male"));
+        //enter First name
+        type(By.cssSelector("#FirstName"), firstName);
+        //enter Last name
+        type(By.cssSelector("#LastName"), lastName);
+        //enter email
+        type(By.cssSelector("#Email"),"rohach" + i + "@gmail.com");
+        //enter password
+        type(By.cssSelector("#Password"), password);
+        //confirm password
+        type(By.cssSelector("#ConfirmPassword"), password);
+    }
+
+    public void clickOnRegisterLink() {
+        click(By.cssSelector("[href='/register']"));
+    }
+
+    public void clickOnLoginButton() {
+        //click on login button
+        driver.findElement(By.cssSelector("input[value='Log in']")).click();
+    }
+
+    public void fillLoginInfo(User user) {
+        //enter email
+        type(By.cssSelector("#Email"), user.getEmail());
+        //enter password
+        type(By.cssSelector("#Password"), user.getPassword());
+    }
+
+    public void clickOnLoginLink() {
+        //click on login link
+        driver.findElement(By.cssSelector(".ico-login")).click();
+    }
+
+    public void pause(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void clickOnCartLink() {
+        click(By.xpath("//*[@id='topcartlink']"));
+    }
+
+    public void clickOnAddToCartButton() {
+        click(By.xpath("(//input[@value='Add to cart'])[2]"));
+    }
 }
