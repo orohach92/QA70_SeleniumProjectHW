ee1ba729f8207d8e365d3531d16d338dab803921
orohach92 <rohach.oleksandr@gmail.com>
2025-12-01 20:25:03 +0100

    homework21,22

--- src/test/java/com/demowebshop/test/ItemTest.java
+++ src/test/java/com/demowebshop/test/ItemTest.java
@@ -6,16 +6,18 @@ import com.demowebshop.models.User;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ItemTest extends TestBase {
 
+    @Parameters({"email", "password"})
     @BeforeMethod
-    public void precondition(){
+    public void precondition(String email, String password){
         app.getUser().clickOnLoginLink();
         app.getUser().fillLoginInfo(new User()
-                .setEmail(UserData.email)
-                .setPassword(UserData.password));
+                .setEmail(email)
+                .setPassword(password));
         app.getUser().clickOnLoginButton();
         Assert.assertTrue(app.getUser().isLogoutButtonPresent());
     }
